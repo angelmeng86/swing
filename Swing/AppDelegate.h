16b83432413e0e63bcc9a24bf51e1b6290e0659c
mapple <lwz@xdja.com>
2016-07-14 21:57:55 +0800

    Swing project init

---
+++ Swing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swing
+//
+//  Created by 刘武忠 on 16/7/13.
+//  Copyright © 2016年 zzteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swing/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Swing
+//
+//  Created by 刘武忠 on 16/7/13.
+//  Copyright © 2016年 zzteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
