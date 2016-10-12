e359a34e262971722a3ef548521d48d353f6d727
mapple <lwz@xdja.com>
2016-10-12 20:29:09 +0800

    1、推送事件修改

--- Swing/AppDelegate.m
+++ Swing/AppDelegate.m
@@ -12,6 +12,8 @@
 
 @interface AppDelegate ()
 
+@property (nonatomic) BOOL isBackground;
+
 @end
 
 @implementation AppDelegate
@@ -152,7 +154,7 @@
     NSString* alert = [[userInfo objectForKey:@"aps"] objectForKey:@"alert"];
     // 把应用右上角的图标​去掉
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
-    if (active && [GlobalCache shareInstance].info) {
+    if (active && [GlobalCache shareInstance].info && !_isBackground) {
         [Fun showMessageBoxWithTitle:@"Notification" andMessage:alert];
     }
     else {
@@ -161,24 +163,26 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    NSLog(@"applicationWillResignActive");
+//    NSLog(@"applicationWillResignActive");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    NSLog(@"applicationDidEnterBackground");
+//    NSLog(@"applicationDidEnterBackground");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     NSLog(@"applicationWillEnterForeground");
+    _isBackground = YES;
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     NSLog(@"applicationDidBecomeActive");
+    _isBackground = NO;
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
